4399952ab800a59da1c022a5e74bb7ba12835870
董冠男 <1752472377@qq.com>
2026-08-18 10:42:42 +0800

    sanity.cli.ts 绑定线上部署 App ID（sanity deploy 生成）

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -6,6 +6,7 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
+    appId: 'xwphevl2nk0ro4452sdwyb7t', // 👈 加上这行，绑定线上部署的 App ID
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
